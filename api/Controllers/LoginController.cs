ec020e2ee18171283f4de9f5949eddbd30529a35
Kalmykov Ivan <admin@kalmykov-group.ru>
2025-04-24 08:12:48 +0000

    initial

---
+++ api/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
